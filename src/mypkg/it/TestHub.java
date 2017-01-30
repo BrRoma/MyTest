f2e194785a6d3c14084c300b0572d90d5e1b4871
Roman <shulga.roman@gtedx.com>
2017-01-30 16:04:48 +0200

    commit by dev

--- src/mypkg/it/TestHub.java
+++ src/mypkg/it/TestHub.java
@@ -4,6 +4,6 @@ package mypkg.it;
 public class TestHub {
 
     public static void main(String[] args) {
-        System.out.println("hi from brad...");
+        System.out.println("hi from dev...");
     }
 }
